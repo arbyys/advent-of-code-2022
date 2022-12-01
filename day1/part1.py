a260ba14c84f6fbea92961d19b362a62d5a42721
arbyys <adam.petricek@centrum.cz>
2022-12-02 00:31:39 +0100

    day1

--- day1/part1.py
+++ day1/part1.py
@@ -3,4 +3,15 @@ from aocd import get_data
 data = get_data(day=1, year=2022)
 data = data.splitlines()
 
-print(data)
+max = -1
+current = 0
+for i in data:
+    if (i == ""):
+        if (current > max):
+            max = current
+        current = 0
+        continue
+    i = int(i)
+    current += i
+
+print(max)
--- day1/part2.py
+++ day1/part2.py
@@ -3,4 +3,16 @@ from aocd import get_data
 data = get_data(day=1, year=2022)
 data = data.splitlines()
 
-print(data)
+max = -1
+result = []
+current = 0
+for i in data:
+    if (i == ""):
+        result.append(current)
+        current = 0
+        continue
+    i = int(i)
+    current += i
+
+result.sort(reverse=True)
+print(result[0] + result[1] + result[2])
