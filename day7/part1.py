30aee02c2ee9a158a86bd4de8eb6804bddb9d76c
arbyys <adam.petricek@centrum.cz>
2022-12-07 22:50:47 +0100

    day7

--- day7/part1.py
+++ day7/part1.py
@@ -1,10 +1,54 @@
+from dataclasses import dataclass
+
 from aocd import get_data
 
-data = get_data(day=1, year=2022)
+data = get_data(day=7, year=2022)
 data = data.splitlines()
-#data = [*map(int, data)]
 
-# with open("template/input.txt") as file:
-#   data = [line.rstrip("\n") for line in file]
+MINIMAL_SIZE = 100000
+total = 0
 
-print(data)
+
+class Directory:
+    def __init__(self, parent=None):
+        self.items = {}
+        self.totalSize = 0
+        self.parent = parent
+        pass
+
+    def addItem(self, name, item):
+        self.items[name] = item
+
+    def calculateSize(self):
+        global total
+        for key, value in self.items.items():
+            if (isinstance(value, Directory)):
+                value.calculateSize()
+                if (value.totalSize <= MINIMAL_SIZE):
+                    total += value.totalSize
+                self.totalSize += value.totalSize
+            else:
+                self.totalSize += int(value)
+
+
+root = Directory()
+current = root
+del data[0]
+
+for index, line in enumerate(data):
+    if (line.startswith("$")):
+        command = line.lstrip("$ ").split(" ")
+        if (command[0] == "cd"):
+            if (command[1] == ".."):
+                current = current.parent
+            else:
+                current = current.items[command[1]]
+    else:
+        file = line.split(" ")
+        if (file[0] == "dir"):
+            current.addItem(file[1], Directory(current))
+        else:
+            current.addItem(file[1], file[0])
+
+root.calculateSize()
+print(total)
--- day7/part2.py
+++ day7/part2.py
@@ -1,10 +1,57 @@
+from dataclasses import dataclass
+
 from aocd import get_data
 
-data = get_data(day=1, year=2022)
+data = get_data(day=7, year=2022)
 data = data.splitlines()
-#data = [*map(int, data)]
 
-# with open("template/input.txt") as file:
-#   data = [line.rstrip("\n") for line in file]
+TOTAL_SIZE = 70000000
+MINIMUM_SIZE = 30000000
+candidateSizes = []
 
-print(data)
+
+class Directory:
+    def __init__(self, parent=None):
+        self.items = {}
+        self.totalSize = 0
+        self.parent = parent
+        pass
+
+    def addItem(self, name, item):
+        self.items[name] = item
+
+    def calculateSize(self):
+        global candidateSizes
+        for key, value in self.items.items():
+            if (isinstance(value, Directory)):
+                value.calculateSize()
+                candidateSizes.append(value.totalSize)
+                self.totalSize += value.totalSize
+            else:
+                self.totalSize += int(value)
+
+
+root = Directory()
+current = root
+del data[0]
+
+for index, line in enumerate(data):
+    if (line.startswith("$")):
+        command = line.lstrip("$ ").split(" ")
+        if (command[0] == "cd"):
+            if (command[1] == ".."):
+                current = current.parent
+            else:
+                current = current.items[command[1]]
+    else:
+        file = line.split(" ")
+        if (file[0] == "dir"):
+            current.addItem(file[1], Directory(current))
+        else:
+            current.addItem(file[1], file[0])
+
+root.calculateSize()
+minimumPossible = MINIMUM_SIZE - (TOTAL_SIZE - root.totalSize)
+candidateSizes = list(filter(lambda x: (x >= minimumPossible), candidateSizes))
+candidateSizes.sort()
+print(candidateSizes[0])
