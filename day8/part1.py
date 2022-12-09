6b2d6db12a086c9fc4ca0c324762517a02991e0f
Adam Petříček <e_petricek@jablotron.cz>
2022-12-09 14:08:08 +0100

    day8 done + day9 files

---
+++ day8/part1.py
@@ -0,0 +1,62 @@
+from aocd import get_data
+
+from enum import Enum
+
+
+class Direction(Enum):
+    TOP = 1
+    RIGHT = 2
+    BOTTOM = 3
+    LEFT = 4
+
+
+data = get_data(day=8, year=2022)
+data = data.splitlines()
+for index, i in enumerate(data):
+    data[index] = [*map(int, i)]
+
+
+def goInDirection(start_i, start_j, direction: Direction):
+    global data
+    current_i = start_i
+    current_j = start_j
+    startElement = data[current_i][current_j]
+
+    match direction:
+        case Direction.TOP:
+            while(current_i > 0):
+                current_i -= 1
+                if(data[current_i][current_j] >= startElement):
+                    return False
+        case Direction.RIGHT:
+            while(current_j < len(data[0])-1):
+                current_j += 1
+                if(data[current_i][current_j] >= startElement):
+                    return False
+        case Direction.BOTTOM:
+            while(current_i < len(data)-1):
+                current_i += 1
+                if(data[current_i][current_j] >= startElement):
+                    return False
+        case Direction.LEFT:
+            while(current_j > 0):
+                current_j -= 1
+                if(data[current_i][current_j] >= startElement):
+                    return False
+    return True
+
+
+total = 0
+for i, row in enumerate(data):
+    for j, col in enumerate(row):
+        currentItem = data[i][j]
+        if (i in [0, len(data)-1] or j in [0, len(row)-1]):
+            total += 1
+            continue
+
+        for direction in Direction:
+            if(goInDirection(i, j, direction)):
+                total += 1
+                break
+
+print(total)
---
+++ day8/part2.py
@@ -0,0 +1,73 @@
+from aocd import get_data
+
+from enum import Enum
+
+
+class Direction(Enum):
+    TOP = 1
+    RIGHT = 2
+    BOTTOM = 3
+    LEFT = 4
+
+
+data = get_data(day=8, year=2022)
+data = data.splitlines()
+for index, i in enumerate(data):
+    data[index] = [*map(int, i)]
+
+
+def goInDirection(start_i, start_j, direction: Direction):
+    global data
+    current_i = start_i
+    current_j = start_j
+    startElement = data[current_i][current_j]
+    trees = 0
+
+    match direction:
+        case Direction.TOP:
+            while(current_i > 0):
+                trees += 1
+                current_i -= 1
+                if(data[current_i][current_j] >= startElement):
+                    return trees
+        case Direction.RIGHT:
+            while(current_j < len(data[0])-1):
+                trees += 1
+                current_j += 1
+                if(data[current_i][current_j] >= startElement):
+                    return trees
+        case Direction.BOTTOM:
+            while(current_i < len(data)-1):
+                trees += 1
+                current_i += 1
+                if(data[current_i][current_j] >= startElement):
+                    return trees
+        case Direction.LEFT:
+            while(current_j > 0):
+                trees += 1
+                current_j -= 1
+                if(data[current_i][current_j] >= startElement):
+                    return trees
+    return trees
+
+
+def calculateScenic(i, j):
+    total = 1
+    for direction in Direction:
+        treesUntilBlock = goInDirection(i, j, direction)
+        total *= treesUntilBlock
+    return total
+
+
+best = -1
+
+for i, row in enumerate(data):
+    for j, col in enumerate(row):
+        currentItem = data[i][j]
+
+        for direction in Direction:
+            result = calculateScenic(i, j)
+            if (result > best):
+                best = result
+
+print(best)
