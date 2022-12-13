b7ced38795b1f53995d94fb6748c696658ae6ddd
arbyys <adam.petricek@centrum.cz>
2022-12-13 13:51:58 +0100

    day12 files

---
+++ day12/part1.py
@@ -0,0 +1,12 @@
+from aocd import get_data
+
+data = get_data(day=1, year=2022)
+data = data.splitlines()
+#data = [*map(int, data)]
+
+with open("template/input.txt") as file:
+    data = [line.rstrip("\n") for line in file]
+
+
+
+print(data)
